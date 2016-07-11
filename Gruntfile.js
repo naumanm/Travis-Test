58eb86e9d408f0001367d17f4a4bdf8a1f47c778
michael nauman <bodhiandmike@gmail.com>
2016-07-11 14:29:30 -0700

    grunt working now clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,25 +9,4 @@ module.exports = function (grunt) {
 
   grunt.registerTask('test', ['jshint']);
 
-/*
-  grunt.registerTask('deploy-edge', function() {
-    grunt.config.merge({
-      config: {
-        aws_s3_path: 'edge',
-        versionPath: '/<%= pkg.version %>.<%= meta.revision %>'
-      }
-    });
-    grunt.task.run(['revision', 'replace:base', 'compress', 'aws_s3:default', 'aws_s3:rootHTML']);
-  });
-
-  grunt.registerTask('deploy-release', function() {
-    grunt.config.merge({
-      config: {
-        aws_s3_path: 'release',
-        versionPath: '/<%= pkg.version %>'
-      }
-    });
-    grunt.task.run(['revision', 'replace:base', 'compress', 'aws_s3:default']);
-  });
-*/
 };
