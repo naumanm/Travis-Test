fd702a78f2da400895477152a9d002c2786644c7
michael nauman <bodhiandmike@gmail.com>
2016-07-12 13:58:35 -0700

    fixed grunt case issue

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: require('./Grunt/jshint'),
-    aws_s3: require('./grunt/aws_s3')
+    aws_s3: require('./Grunt/aws_s3')
   });
 
   grunt.registerTask('test', ['jshint']);
